3aa2b7d0cb0d858d59c3aff8605e5be2b89ce31b
johnmfajardo40 <johnmfajardo40@gmail.com>
2024-10-04 09:55:08 +0800

    activity

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	cout << "OUR LADY OF LOURDES COLLEGE FOUNDATION" << endl;
+	cout << "COMPUTER STUDIES" << endl;
+	cout << "BSIT 1st Year" << endl;
+	cout << "IVAN T. LUMENARIO" << endl;
+	cout << "P-1 DAGUIT LABO CAMARINES NORTE" << endl;
+	
+	return 0;
+}
